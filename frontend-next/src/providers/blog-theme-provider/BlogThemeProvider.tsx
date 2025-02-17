d720143d17c2d3cf717b383cf7bf8a89bc7bb64a
Andrii Kryvda <kryvdaandrei@gmail.com>
2025-02-17 22:34:15 +0100

    added mui cssBaseline to theme provider

--- frontend-next/src/providers/blog-theme-provider/BlogThemeProvider.tsx
+++ frontend-next/src/providers/blog-theme-provider/BlogThemeProvider.tsx
@@ -1,5 +1,6 @@
 import { overrideComponents } from "@/providers/blog-theme-provider/override-components";
 import { theme } from "@/providers/blog-theme-provider/useDefaultTheme";
+import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { PropsWithChildren } from "react";
 interface Props extends PropsWithChildren {
@@ -12,5 +13,10 @@ interface Props extends PropsWithChildren {
 export default function BlogThemeProvider({ children }: Props) {
   theme.components = overrideComponents();
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
 }
