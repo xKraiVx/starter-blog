9ec598c004ac2ad4ac639ddd0193299e996ebf25
Andrii Kryvda <kryvdaandrei@gmail.com>
2025-03-22 20:42:14 +0100

    output information on profile page

--- frontend-next/src/ssr-features/pages/profile/Profile.tsx
+++ frontend-next/src/ssr-features/pages/profile/Profile.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import UiPageContainer from "@/common/components/ui/ui-section-container/UiSectionContainer";
 import UiPageTitle from "@/common/components/ui/ui-page-title/UiPageTitle";
 import { JSX } from "react";
+import { useGetMe } from "@/common/hooks/useGetMe";
+import { Box, Typography } from "@mui/material";
 
 export default function Profile(): JSX.Element {
+  const { data } = useGetMe();
+
   return (
     <UiPageContainer>
       <UiPageTitle>Profile</UiPageTitle>
+      <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+        <Typography variant="bodyXl">ID:</Typography>
+        <Typography variant="subtitle1">{data?.id}</Typography>
+      </Box>
+      <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+        <Typography variant="bodyXl">Email:</Typography>
+        <Typography variant="subtitle1">{data?.email}</Typography>
+      </Box>
+      <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+        <Typography variant="bodyXl">Role:</Typography>
+        <Typography variant="subtitle1">{data?.role?.name}</Typography>
+      </Box>
     </UiPageContainer>
   );
 }
