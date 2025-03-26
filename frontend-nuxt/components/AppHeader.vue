b9ce9b9e9573d0f0967e0379ecdec9375959bc53
Andrii Kryvda <kryvdaandrei@gmail.com>
2025-03-26 23:40:33 +0100

    updated layout and pages

---
+++ frontend-nuxt/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
---
+++ frontend-nuxt/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to My Blog</h1>
+    <p>This is the home page of my starter blog built with Nuxt.js.</p>
+  </div>
+</template>
